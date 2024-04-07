b5cc8565d7a8773b5b886223b00f6455bc87c8f3
SonaliPatankar <sonali.patankar@cctech.co.in>
2024-04-07 21:46:30 +0530

    stage

---
+++ FileTranslator/ObjToStl/Program.cs
@@ -0,0 +1,38 @@
+﻿using ComputationalGeometry;
+using IOOperation;
+using System.Xml;
+using Conversion;
+
+namespace ObjToStl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string baseDirectory = "D:\\sonali_workspace\\C#\\FileTranslator\\ObjToStl\\Resources\\";
+                string baseDirectory1 = "D:\\sonali_workspace\\C#\\FileTranslator\\ObjToStl\\OutputFiles\\";
+
+                Console.WriteLine("Enter the relative path to the input OBJ file:");
+                string inputRelativePath = Console.ReadLine();
+                string inputFilePath = System.IO.Path.Combine(baseDirectory, inputRelativePath);
+
+                Console.WriteLine("Enter the relative path to the output STL file:");
+                string outputRelativePath = Console.ReadLine();
+                string outputFilePath = System.IO.Path.Combine(baseDirectory1, outputRelativePath);
+
+                Triangulation triangulation = new Triangulation();
+                ObjReader objReader = new ObjReader();
+                objReader.ReadOBJ(inputFilePath, triangulation);
+
+                ObjConvertor objToStl = new ObjConvertor();
+                objToStl.ObjToStlConversion(outputFilePath, triangulation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
